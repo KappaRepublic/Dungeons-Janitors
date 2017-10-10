3fc597db0d0bb29369dcf2eed165a04f280665d8
Paul Drauz-Brown <paul.drauzbrown@icloud.com>
2017-10-10 09:22:13 +0100

    Wwise Integration
    Added wwise and new project

---
+++ TorchLightersBuild/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,61 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID CLEANUP_CORPSE = 1545137546U;
+        static const AkUniqueID CLOSE_GATE = 4197195035U;
+        static const AkUniqueID FOOTSTEP = 1866025847U;
+        static const AkUniqueID LEVEL_1 = 1290008369U;
+        static const AkUniqueID LEVEL_2 = 1290008370U;
+        static const AkUniqueID LEVEL_3 = 1290008371U;
+        static const AkUniqueID LEVEL_4 = 1290008372U;
+        static const AkUniqueID LEVEL_COMPLETE = 3736098925U;
+        static const AkUniqueID LEVEL_START = 352576276U;
+        static const AkUniqueID LIGHT_TORCH = 2156420204U;
+        static const AkUniqueID OPEN_GATE = 196301345U;
+        static const AkUniqueID PULL_LEVER = 766957497U;
+        static const AkUniqueID SET_TRAP = 2733576287U;
+        static const AkUniqueID SPIKES = 747415224U;
+        static const AkUniqueID TRIGGER_TRAP = 4012165883U;
+    } // namespace EVENTS
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID SS_AIR_FEAR = 1351367891U;
+        static const AkUniqueID SS_AIR_FREEFALL = 3002758120U;
+        static const AkUniqueID SS_AIR_FURY = 1029930033U;
+        static const AkUniqueID SS_AIR_MONTH = 2648548617U;
+        static const AkUniqueID SS_AIR_PRESENCE = 3847924954U;
+        static const AkUniqueID SS_AIR_RPM = 822163944U;
+        static const AkUniqueID SS_AIR_SIZE = 3074696722U;
+        static const AkUniqueID SS_AIR_STORM = 3715662592U;
+        static const AkUniqueID SS_AIR_TIMEOFDAY = 3203397129U;
+        static const AkUniqueID SS_AIR_TURBULENCE = 4160247818U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID MAIN = 3161908922U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
